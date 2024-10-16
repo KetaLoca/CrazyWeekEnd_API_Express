e8be741464829ef426c8836a8bee7cdc34e8a7a1
ketaloca <xesuspb@gmail.com>
2024-10-16 11:46:44 +0200

    Middlewares json y cors añadidos

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-import express from "express"
+import express, { json } from "express"
+import cors from 'cors'
 
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
+
+app.use(json())
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log("Server listening on PORT " + PORT)
