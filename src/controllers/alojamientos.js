d92e2f418a01cf9e2973de463004cad9db043fff
ketaloca <xesuspb@gmail.com>
2024-10-21 10:55:55 +0200

    Terminando esqueleto MVC *alojamientos*

---
+++ src/controllers/alojamientos.js
@@ -0,0 +1,21 @@
+import { AlojamientosModel } from "../models/alojamientos";
+import { validateAlojamiento, validatePartialAlojamiento } from "../schemas/alojamiento";
+
+export class AlojamientosController {
+
+    static getAlojamientos() {
+
+    }
+
+    static getAlojamientoById() {
+
+    }
+
+    static create() {
+
+    }
+
+    static delete() {
+
+    }
+}
---
+++ src/models/alojamientos.js
@@ -0,0 +1,20 @@
+import db from '../../knexDB.js'
+
+export class AlojamientosModel {
+
+    static getAll() {
+
+    }
+
+    static getById() {
+
+    }
+
+    static create() {
+
+    }
+
+    static delete() {
+
+    }
+}
