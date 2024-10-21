406ca11e5d49fcbbe2ace5de3ed59c8694e5f88b
ketaloca <xesuspb@gmail.com>
2024-10-21 12:16:35 +0200

    Logica modelo terminada. MVC de alojamientos funcionando(aún falta testear los endpoints)

--- src/models/alojamientos.js
+++ src/models/alojamientos.js
@@ -3,23 +3,27 @@ import db from '../../knexDB.js'
 export class AlojamientosModel {
 
     static async getAll() {
-        const alojamientos = db('CrazyWeekEnd.alojamientos').select('*')
+        const alojamientos = await db('CrazyWeekEnd.alojamientos').select('*')
         return alojamientos
     }
 
     static async getByEmail(email) {
-
+        const alojamientos = await db('CrazyWeekEnd.alojamientos').where('userEmail', email).select('*')
+        return alojamientos
     }
 
     static async getById(id) {
-
+        const alojamiento = await db('CrazyWeekEnd.alojamientos').where('id', id).first.select('*')
+        return alojamiento
     }
 
     static async create(input) {
-
+        const result = await db('CrazyWeekEnd.alojamientos').insert(input).returning('*')
+        return result
     }
 
     static async delete(id) {
-
+        const result = await db('CrazyWeekEnd.alojamientos').where('id', id).del()
+        return result
     }
 }
