cfc4fa7762c385fc1d0caeac1f7248b77c207aaa
ketaloca <xesuspb@gmail.com>
2024-10-25 14:13:38 +0200

    Registro y Login funcionando(faltan los jsonwebtokens)

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -33,7 +33,21 @@ export class UserController {
     }
 
     static login = async (req, res) => {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Faltan el email o la password' })
+        }
 
+        const user = await UserModel.getById(email)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+
+        const comprobar = await bcrypt.compare(password, user.password)
+        if (comprobar) {
+            return res.status(200).json({ message: 'Login correcto' })
+        }
+        res.status(401).json({ message: 'Password incorrecta' })
     }
 
     static update = async (req, res) => {
