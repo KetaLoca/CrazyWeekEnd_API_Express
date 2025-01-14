23d2f3296b24395c45209615ffaef1dde53976e3
ketaloca <xesuspb@gmail.com>
2025-01-14 14:38:46 +0100

    .

--- knexdb.js
+++ knexdb.js
@@ -3,7 +3,7 @@ import knex from "knex";
 const db = knex({
     client: 'pg',
     connection: {
-        host: "localhost",
+        host: "host.docker.internal",
         port: "5432",
         user: "postgres",
         password: "1234",
