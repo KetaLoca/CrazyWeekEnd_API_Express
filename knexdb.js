8b44924079dbee13475abd18e38b940f62c9cbd1
ketaloca <xesuspb@gmail.com>
2025-01-16 22:02:48 +0100

    Docker compose funcionando totalmente en el backend

--- knexdb.js
+++ knexdb.js
@@ -3,8 +3,8 @@ import knex from "knex";
 const db = knex({
     client: 'pg',
     connection: {
-        host: "localhost",
-        port: "5433",
+        host: "postgres",
+        port: "5432",
         user: "postgres",
         password: "1234",
         database: "postgres",
